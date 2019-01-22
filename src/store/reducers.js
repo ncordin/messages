cb617522d0b64c84f8466b1f81f9960b9e3217c0
Nicolas Cordin <dindonus@users.noreply.github.com>
2019-01-23 00:27:26 +0700

    Remove redux

--- src/store/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import messages from './messages/messages.reducer';
-
-const rootReducer = combineReducers({
-  messages,
-});
-
-export default rootReducer;
